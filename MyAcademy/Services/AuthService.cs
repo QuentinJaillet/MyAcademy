550d72b4af5f0fbb1a55b52196155bf940adb744
QuentinJaillet <quentin.jaillet@gmail.com>
2025-02-21 08:47:10 +0100

    Add Course Service

--- MyAcademy/Services/AuthService.cs
+++ MyAcademy/Services/AuthService.cs
@@ -1,5 +1,4 @@
 using System.Net.Http.Headers;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 
