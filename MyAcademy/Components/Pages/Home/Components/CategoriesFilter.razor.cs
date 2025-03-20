f5dc12ee2db2a8a75ba8d2df2335af4b541e46bb
QuentinJaillet <quentin.jaillet@gmail.com>
2025-03-20 16:28:10 +0100

    Un peu de Blazor

---
+++ MyAcademy/Components/Pages/Home/Components/CategoriesFilter.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using MyAcademy.Models;
+
+namespace MyAcademy.Components.Pages.Home.Components;
+
+public partial class CategoriesFilter
+{
+    [Parameter]
+    public IEnumerable<Category>? Categories { get; set; }
+    
+    private IDictionary<Models.Category, bool> CategoriesSelected { get; set; } = new Dictionary<Models.Category, bool>();
+    
+    protected override void OnParametersSet()
+    {
+        if (Categories != null) 
+            CategoriesSelected = Categories.ToDictionary(category => category, _ => false);
+    }
+    
+    private void CheckboxChanged(ChangeEventArgs e)
+    {
+        // get the checkbox state
+        var value = e.Value;
+        Console.WriteLine($"Checkbox changed {value}");
+        Console.WriteLine(e);
+    }
+}
--- MyAcademy/Components/Pages/Home/Home.razor.cs
+++ MyAcademy/Components/Pages/Home/Home.razor.cs
@@ -1,37 +1,42 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MyAcademy.Models;
 using MyAcademy.Services;
 
-namespace MyAcademy.Components.Pages;
+namespace MyAcademy.Components.Pages.Home;
 
 public partial class Home
 {
-    [Inject] public CourseService CourseService { get; set; }
+    [Inject]
+    public CourseService CourseService { get; set; }
 
-    [Inject] public ILogger<Home> Logger { get; set; }
+    [Inject]
+    public ILogger<Home> Logger { get; set; }
 
     public string ErrorMessage { get; set; } = string.Empty;
     public IEnumerable<Course> Summaries { get; set; } = [];
-    public Dictionary<Models.Category, bool> CategoriesSelected { get; set; } = new Dictionary<Models.Category, bool>();
+    public IEnumerable<Category>? Categories { get; set; } = [];
     public string SearchTerm { get; set; }
-    
+
     protected override async Task OnInitializedAsync()
     {
         try
         {
-            var categories = await CourseService.GetCategoriesAsync().ConfigureAwait(false);
+            //Categories = await CourseService.GetCategoriesAsync().ConfigureAwait(false);
 
-            foreach (var category in categories)
+            Categories = new List<Category>()
             {
-                CategoriesSelected.Add(category, false);
-            }
+                new Category(Guid.NewGuid(), "Développement"),
+                new Category(Guid.NewGuid(), "Design"),
+                new Category(Guid.NewGuid(), "Marketing"),
+                new Category(Guid.NewGuid(), "Business"),
+            };
         }
         catch (Exception e)
         {
             Logger.LogError(e, "Error de chargement des catégories");
             //ErrorMessage = "Une erreur est survenue lors du chargement des cours.";
         }
-
 
         try
         {
@@ -60,21 +65,21 @@ public partial class Home
 
     private Task FilterByCategory(Models.Category category)
     {
-        CategoriesSelected[category] = !CategoriesSelected[category];
+        //CategoriesSelected[category] = !CategoriesSelected[category];
 
         Filter();
 
         return Task.CompletedTask;
     }
 
-    private void Filter()
+    private void Filter() 
     {
         // Récupérère les catégories sélectionnées
-        var selectedCategories = CategoriesSelected
+        /*var selectedCategories = CategoriesSelected
             .Where(x => x.Value)
             .Select(x => x.Key)
-            .ToList();
+            .ToList();*/
 
-        Console.WriteLine($"Selected categories: {string.Join(", ", selectedCategories.Select(x => x.Name))}");
+        //Console.WriteLine($"Selected categories: {string.Join(", ", selectedCategories.Select(x => x.Name))}");
     }
 }
